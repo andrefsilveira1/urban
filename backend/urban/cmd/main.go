20ff9f6d3ec294fe13c6de4652b2ec1fb5486814
André Freitas <freitasandre38@gmail.com>
2024-03-24 16:31:16 -0300

    feat: create main shutdown service

--- backend/urban/cmd/main.go
+++ backend/urban/cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("WORKING")
+	fmt.Println("Starting server...")
+	// var path string // database variables will got be here
+
+	// put repositories
+
+	// Shutdown
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	defer signal.Stop(interrupt)
+
 }
