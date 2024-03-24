408a7ae2e03d2423a8f7779ef43050f97bedf8e6
André Freitas <freitasandre38@gmail.com>
2024-03-24 17:32:30 -0300

    refactor: change auth middleware to include jwt token

--- backend/urban/internal/transport/rest/middleware/auth.go
+++ backend/urban/internal/transport/rest/middleware/auth.go
@@ -1,15 +1,36 @@
 package middleware
 
 import (
-	"fmt"
-
+	"github.com/andrefsilveira1/urban/internal/domain"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber"
 )
 
 func Auth(c *fiber.Ctx) error {
+	var secret = []byte("my-scecret-key") // change later
 	header := c.Get("Authorization")
-	fmt.Println("HEADER:", header)
-	// TODO
+	if header == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Missing Token Header",
+		})
+	}
+	tokenString := header[7:]
+	claims := domain.Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return secret, nil
+	})
+
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Invalid token",
+		})
+	}
+
+	if !token.Valid {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Invalid token",
+		})
+	}
 
 	return nil
 }
