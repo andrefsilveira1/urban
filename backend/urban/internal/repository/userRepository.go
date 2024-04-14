bad60d1aba6652e3df3ecb8982c6120fed8d7e18
André Freitas <freitasandre38@gmail.com>
2024-04-14 15:12:50 -0300

    feat: create user repository

--- backend/urban/internal/repository/userRepository.go
+++ backend/urban/internal/repository/userRepository.go
@@ -1,7 +1,30 @@
 package repository
 
+type User struct {
+	ID       string
+	Name     string
+	Email    string
+	Password string
+}
+
 type UserRepository interface {
 	Save(user *User) error
 	Get(id string) (*User, error)
 	// more methods
+}
+
+type ScyllaRepository struct {
+	// Scylla init
+}
+
+func (r *ScyllaRepository) Save(User *User) error {
+	// Save logic
+
+	return nil
+}
+
+func (r *ScyllaRepository) Get(id string) (*User, error) {
+	// Get logic
+
+	return nil, nil
 }
