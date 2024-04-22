929f3810a31312632d032569c4b91f814d040480
André Freitas <freitasandre38@gmail.com>
2024-04-22 19:20:11 -0300

    feat: create image repository methods using scylla cql

--- backend/urban/internal/repository/scylla/image_repository.go
+++ backend/urban/internal/repository/scylla/image_repository.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/andrefsilveira1/urban/internal/domain/models"
+import (
+	"fmt"
+
+	"github.com/andrefsilveira1/urban/internal/domain/models"
+	"github.com/gocql/gocql"
+)
 
 type ImageRepository interface {
 	Save(image *models.Image) error
@@ -9,4 +14,64 @@ type ImageRepository interface {
 	Delete(id string) error
 	HashImage(image *models.Image) (*[]models.Image, error)
 	// ...
+}
+
+func NewScyllaImageRepository(session *gocql.Session) *ScyllaRepository {
+	return &ScyllaRepository{
+		session: session,
+	}
+}
+
+func (r *ScyllaRepository) Save(image *models.Image) error {
+	query := "INSERT INTO images (id, name, date, content) VALUES (?, ?, ?, ?)"
+	if err := r.session.Query(query, image.Id, image.Name, image.Date, image.Content).Exec(); err != nil {
+		return fmt.Errorf("Error: Saving image has failed: $v", err)
+	}
+	return nil
+}
+
+func (r *ScyllaRepository) Get(id string) (*models.Image, error) {
+	var image models.Image
+
+	query := "SELECT id, name, date, content FROM images WHERE id = ? LIMIT 1"
+	if err := r.session.Query(query, id).Scan(&image.Id, &image.Name, &image.Date, &image.Content); err != nil {
+		return nil, fmt.Errorf("Error: Saving image has failed: $v", err)
+	}
+
+	return &image, nil
+}
+
+func (r *ScyllaRepository) List() ([]models.Image, error) {
+	var images []models.Image
+	query := "SELECT id, name, date, content FROM images"
+	iter := r.session.Query(query).Iter()
+	defer iter.Close()
+
+	var id, name string
+	var date gocql.UUID
+	var content []byte
+
+	for iter.Scan(&id, &name, &date, &content) {
+		images = append(images, models.Image{
+			Id:      id,
+			Name:    name,
+			Date:    date.Time(),
+			Content: content,
+		})
+	}
+
+	if err := iter.Close(); err != nil {
+		return nil, fmt.Errorf("Error while listing images: %v", err)
+	}
+
+	return images, nil
+
+}
+
+func (r *ScyllaRepository) Delete(id string) error {
+	query := "DELETE FROM images WHERE id = ?"
+	if err := r.session.Query(query, id).Exec(); err != nil {
+		return fmt.Errorf("error deleting image: %v", err)
+	}
+	return nil
 }
