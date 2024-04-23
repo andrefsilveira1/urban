aed4e9816aab86b10019a98f531a562217ef1890
André Freitas <freitasandre38@gmail.com>
2024-04-23 19:18:53 -0300

    feat: create shutdown function

--- backend/urban/internal/transport/rest/server.go
+++ backend/urban/internal/transport/rest/server.go
@@ -1,12 +1,19 @@
 package rest
 
 import (
+	"context"
+	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/andrefsilveira1/urban/internal/domain"
 	"github.com/andrefsilveira1/urban/internal/transport/rest/routes"
 	"github.com/gofiber/fiber/v2"
 )
+
+type Server struct {
+	app *fiber.App
+}
 
 func Start(port int, userService *domain.UserService) error {
 	app := fiber.New()
@@ -17,4 +24,15 @@ func Start(port int, userService *domain.UserService) error {
 	})
 
 	return app.Listen(":" + strconv.Itoa(port))
+}
+
+func (s *Server) Stop(timeout time.Duration) error {
+	_, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	if err := s.app.Shutdown(); err != nil {
+		return fmt.Errorf("failed to gracefully shutdown server: %v", err)
+	}
+
+	return nil
 }
