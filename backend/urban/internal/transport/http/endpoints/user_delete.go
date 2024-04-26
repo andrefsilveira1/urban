48d521160a0e63e0c02806e5cbc1cb224d769f6c
André Freitas <freitasandre38@gmail.com>
2024-04-26 08:51:15 -0300

    feat: create user endpoints for handler

---
+++ backend/urban/internal/transport/http/endpoints/user_delete.go
@@ -0,0 +1,14 @@
+package endpoints
+
+import (
+	"net/http"
+
+	"github.com/andrefsilveira1/urban/internal/domain"
+)
+
+func MakeDeleteUserEndpoint(itemService *domain.ImageService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, "Endpoint not implemented yet", http.StatusInternalServerError)
+	}
+}
